6afc296d7866aef5972035456c08dfb94cb2b461
Tanishq Gaur <tanishq.gaur07@gmail.com>
2026-01-28 13:30:39 +0530

    .

--- proxy.ts
+++ proxy.ts
@@ -1,7 +1,7 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
 // 1. Define which routes are public (don't need login)
-const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)']);
+const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)', '/(root)/(credits)(.*)']);
 
 export default clerkMiddleware(async (auth, request) => {
   // 2. If the current route is NOT public, protect it
