228aba57de40c77f99b41ff232eb4742691f0b5e
Tanishq Gaur <tanishq.gaur07@gmail.com>
2026-01-28 13:14:34 +0530

    fix some more errors

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  // reactCompiler: true, // Disabled to prevent compilation issues
+  reactCompiler: true,
+  // If you are on Windows, forcing webpack is still safer for v3
+  webpack: (config) => {
+    return config;
+  },
+  
 };
 
 export default nextConfig;
