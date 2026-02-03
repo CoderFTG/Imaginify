4a690ddb6a01aa40097ceb8a5649de0aa48e33fe
Tanishq Gaur <tanishq.gaur07@gmail.com>
2026-02-03 19:41:23 +0530

    Implement credit purchasing with Stripe integration and user profile displaying credit balance and image manipulations.

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,9 @@ import { IBM_Plex_Sans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "@/components/ui/sonner";
 
-const IBMPlex = IBM_Plex_Sans({ 
+const IBMPlex = IBM_Plex_Sans({
   subsets: ["latin"],
   weight: ['400', '500', '600', '700'],
   variable: '--font-ibm-plex'
@@ -21,10 +22,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider appearance={{variables : {colorPrimary : '#624cf5'}}}>
+    <ClerkProvider appearance={{ variables: { colorPrimary: '#624cf5' } }}>
       <html lang="en">
         <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>
           {children}
+          <Toaster />
         </body>
       </html>
     </ClerkProvider>
